82e56b096a06a797d14ee04459c75890640588a3
unknown <saketnagmote@gmail.com>
2019-03-08 23:14:42 +0530

    add

---
+++ seleniumByTechnocredits/src/seleniumPractise/saket/Assignment4.java
@@ -0,0 +1,49 @@
+package seleniumPractise.saket;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import util.Init;
+
+public class Assignment4 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = Init.initChromeDriver();
+		driver.get("http://automationbykrishna.com/#");
+		Thread.sleep(1000);
+
+		driver.manage().window().maximize();
+		driver.findElement(By.id("basicelements")).click();
+		Thread.sleep(2000);
+		WebElement element = driver.findElement(By.xpath("//button[@id='javascriptPromp']"));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView(true)", element);
+ 
+		element.click();
+		Alert alert = driver.switchTo().alert();
+		Thread.sleep(2000);
+		System.out.println("I am in alert");
+		String name="saket";
+		alert.sendKeys(name);
+		System.out.println("I have send the key in alert");
+		alert.accept();
+		
+		String s="Hello saket! How are you today?";
+		
+		if(s.contains(name))
+{
+	System.out.println("succesful");
+	
+}
+				
+				
+		
+
+	}
+
+}
---
+++ seleniumByTechnocredits/src/seleniumPractise/saket/Assignment_1.java
@@ -0,0 +1,62 @@
+package seleniumPractise.saket;
+
+import org.omg.Messaging.SyncScopeHelper;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import util.Init;
+
+public class Assignment_1 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = Init.initChromeDriver();
+		driver.get("http://automationbykrishna.com/#");
+
+		driver.findElement(By.id("basicelements")).click();
+		Thread.sleep(2000);
+
+		driver.findElement(By.id("UserFirstName")).sendKeys("Saket");
+
+		driver.findElement(By.xpath("//input[@id='UserLastName']")).sendKeys("Nagmote");
+
+		driver.findElement(By.xpath("//input[@id='UserCompanyName']")).sendKeys("Capgemini");
+
+		String b = driver.findElement(By.id("UserFirstName")).getAttribute("value");
+		String c = driver.findElement(By.xpath("//input[@id='UserLastName']")).getAttribute("value");
+		String d = driver.findElement(By.xpath("//input[@id='UserCompanyName']")).getAttribute("value");
+
+		driver.findElement(By.xpath("//button[@onclick='myFunctionPopUp()' ]")).click();
+
+		Thread.sleep(2000);
+
+		Alert alert = driver.switchTo().alert();
+
+		String a = alert.getText();
+		System.out.println(a);
+
+		String[] words = a.split("and");
+
+		String input = (b.concat(c).concat(d));
+		System.out.println("Input" + input);
+		String expected = "";
+
+		for (int i = 0; i <= words.length - 1; i++) {
+
+			expected = expected + words[i];
+
+		}
+		System.out.print(expected);
+
+		a = expected.replaceAll("\\s", "");
+
+		System.out.println(a);
+
+		if (a.equals((input))) {
+			System.out.println("Succesfully compared the input with the alert and it is matching");
+		}
+
+	}
+}
